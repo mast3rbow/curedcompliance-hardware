24c6600e37d31c8f9dec7108f172290dc8aa3dec
Bryton Wishart <bryton@curedcompliance.com>
2023-07-15 13:15:40 +1000

    Update main.py

--- files/main.py
+++ files/main.py
@@ -2,6 +2,10 @@
 from time import sleep
 from boot import update
 
+from ble_gateway import scanner
+
 while True:
     sleep(10)
     print("updated for from github for 10 seconds")
+    print("scanning")
+    scanner()
